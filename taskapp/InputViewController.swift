56caddd940bfeed4e981d2ca12b887ef61b868f5
HeuyFreeman <heuyfreekman@gmail.com>
2019-06-19 14:33:49 +0900

    Task.swift作成、inputViewでタスクの登録、変更部分まで

--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -7,27 +7,43 @@
 //
 
 import UIKit
+import RealmSwift
 
 class InputViewController: UIViewController {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentsTextView: UITextView!
     @IBOutlet weak var datePicker: UIDatePicker!
     
+    let realm = try! Realm()
+    var task: Task!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // 背景をタップしたらdismissKeyboardメソッドを呼ぶように設定する
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(dismissKeyboard))
+        //view(背景）にtapGestureを登録する
+        self.view.addGestureRecognizer(tapGesture)
+        
+        titleTextField.text = task.title
+        contentsTextView.text = task.contents
+        datePicker.date = task.date
+    }
+    //Viewが消える時にtaskに登録する。プライマリキー変更などができない
+    override func viewWillDisappear(_ animated: Bool) {
+        try! realm.write {
+            self.task.title = self.titleTextField.text!
+            self.task.contents = self.contentsTextView.text
+            self.task.date = self.datePicker.date
+            self.realm.add(self.task, update: true)
+        }
+        
+        super.viewWillDisappear(animated)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func dismissKeyboard(){
+        // キーボードを閉じる
+        view.endEditing(true)
     }
-    */
 
 }
---
+++ taskapp/Task.swift
@@ -0,0 +1,29 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by Noboru Nishizawa on 2019/06/19.
+//  Copyright © 2019 heuy.freeman. All rights reserved.
+//
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    /// 日時
+    @objc dynamic var date = Date()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
